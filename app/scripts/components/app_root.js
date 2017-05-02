f90cd43fde2de4c820ba0ccdb961bdbba0c4c2a8
Devon Moubry <devon@moubry.com>
2017-05-02 11:54:12 -0500

    add background images
    add font awesome folder

--- app/scripts/components/app_root.js
+++ app/scripts/components/app_root.js
@@ -7,6 +7,10 @@ class AppRoot extends React.Component {
     return (
       <main>
         <section />
+        <img src="app/images/averie-woodard-123973.jpg" />
+        <img src="app/images/aaron-burden-75599.jpg" />
+        <img src="app/images/piyanut-suntaranil-223184.jpg" />
+        <img src="app/images/averie-woodard-123972.jpg" />
       </main>
     );
   }
