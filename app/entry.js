542d2c1726ca1ee28f686a91314d3d6abb17b6e1
Devon Moubry <devon@moubry.com>
2017-05-02 12:22:55 -0500

    add photos to background
    require images in dist

--- app/entry.js
+++ app/entry.js
@@ -1,6 +1,10 @@
 require('file-loader?emitFile=false!./index.html');
 require('./stylesheets/app.scss'); /* This loads the css and foces the sass to compile... */
-require('file-loader?emitFile=false!./images/js-img.png');
+require('file-loader?emitFile=false!./images/averie-woodard-123973.jpg');
+require('file-loader?emitFile=false!./images/piyanut-suntaranil-223184.jpg');
+require('file-loader?emitFile=false!./images/aaron-burden-75599.jpg');
+require('file-loader?emitFile=false!./images/averie-woodard-123972.jpg');
+
 import app from './scripts/app.js';
 
 app();
--- app/scripts/components/app_root.js
+++ app/scripts/components/app_root.js
@@ -6,11 +6,12 @@ class AppRoot extends React.Component {
   render() {
     return (
       <main>
-        <section />
-        <img src="app/images/averie-woodard-123973.jpg" />
-        <img src="app/images/aaron-burden-75599.jpg" />
-        <img src="app/images/piyanut-suntaranil-223184.jpg" />
-        <img src="app/images/averie-woodard-123972.jpg" />
+        <h1>Hey, you.</h1>
+        <p>Henry David Thoreau</p>
+        <img src="../images/averie-woodard-123973.jpg" />
+        <img src="../images/aaron-burden-75599.jpg" />
+        <img src="../images/piyanut-suntaranil-223184.jpg" />
+        <img src="../images/averie-woodard-123972.jpg" />
       </main>
     );
   }
