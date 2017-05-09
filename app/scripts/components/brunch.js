7435f61ff6b09fcb5c55cc72ed0f0787a1b06828
Devon Moubry <devon@moubry.com>
2017-05-09 12:17:47 -0500

    hook up cards to data
    render hike and brunch info pages
    
    update json

--- app/scripts/components/brunch.js
+++ app/scripts/components/brunch.js
@@ -2,26 +2,41 @@ import React from "react";
 import { connect } from "react-redux";
 import { Route, Link, NavLink } from "react-router-dom";
 import container from "../containers/all.js";
+import _ from "lodash";
 
 class BrunchComponent extends React.Component {
   constructor(props) {
     super(props)
 
+
+    this.handleYelpButton = this.handleYelpButton.bind(this)
+    this.getHike = this.getBrunch.bind(this)
   }
+
+  getBrunch(id) {
+    // iterate through this.props.searchResult
+    console.log('brunches', this.props.reducer.foodSearchResults);
+    const brunches = this.props.reducer.foodSearchResults;
+    let theBrunch = _.filter(brunches, { 'id': id });
+    return theBrunch[0];
+  }
+
+  handleYelpButton() {
+    console.log('go to yelp page');
+  }
+
   render() {
+    console.log(this.props.match.params.id);
+    const brunch = this.getBrunch(this.props.match.params.id);
+    console.log('brunch', brunch);
     return (
       <div className="brunch-card-container">
         <h1>Austin Adventures</h1>
-        <p>brunch['name']</p>
-        <p>brunch['city']</p>
-        <p>brunch['state']</p>
-        <p>brunch['zipcode']</p>
-        <p>brunch['phone number']</p>
-        <p>brunch['brunch']</p>
-        <p>brunch['review']</p>
-        <p>brunch['website']</p>
-        {brunch['id']}
-        {brunch['coordinates']}
+        <p><a href={'http://' + brunch['website']} target="_blank">{brunch['name']}</a> - {brunch['city']}, {brunch['state']} {brunch['zipcode']}</p>
+        <p>{brunch['phone number']}</p>
+        <p>{brunch['brunch']}</p>
+        <p>{brunch['review']}</p>
+        <button onClick={this.handleYelpButton} className="yelp-button" type="submit"><i className="fa fa-yelp" aria-hidden="true"></i></button>
       </div>
     );
   }
--- app/scripts/components/hike.js
+++ app/scripts/components/hike.js
@@ -2,30 +2,38 @@ import React from "react";
 import { connect } from "react-redux";
 import { Route, Link, NavLink } from "react-router-dom";
 import container from "../containers/all.js";
+import _ from "lodash";
 
 class HikeComponent extends React.Component {
   constructor(props) {
     super(props)
 
+    this.handleYelpButton = this.handleYelpButton.bind(this)
+    this.getHike = this.getHike.bind(this)
   }
 
   getHike(id) {
-    // iterate through this.propssearcResults
-    //return hike;
+    // iterate through this.props.searchResult
+    const clickId = Number(id);
+    const hikes = this.props.reducer.searchResults;
+    let theHike = _.filter(hikes, { 'unique_id': clickId });
+    return theHike[0];
+  }
+
+  handleYelpButton() {
+    console.log('go to yelp page');
   }
 
   render() {
-    //const hike = getHike(this.props.urlstuff.id);
-
+    const hike = this.getHike(this.props.match.params.id);
+    console.log('hike', hike);
     return (
       <div className="hike-card-container">
         <h1>Austin Adventures</h1>
-        <p>hike['name']</p>
-        <p>hike['city']</p>
-        <p>hike['state']</p>
-        <p>hike['country']</p>
-        <p>hike['directions']</p>
-        <p>hike['description']</p>
+        <p>{hike['name']} - {hike['city']}, {hike['state']}</p>
+        <p>{hike['directions']}</p>
+        <p>{hike['description']}</p>
+        <button onClick={this.handleYelpButton} className="yelp-button" type="submit"><i className="fa fa-yelp" aria-hidden="true"></i></button>
       </div>
     );
   }
