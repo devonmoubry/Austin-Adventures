df17045d1b13c1b5f80f66b3aaabbb64982da603
Devon Moubry <devon@moubry.com>
2017-05-09 17:58:06 -0500

    Add map to brunch card

--- app/scripts/components/brunch.js
+++ app/scripts/components/brunch.js
@@ -3,19 +3,27 @@ import { connect } from "react-redux";
 import { Route, Link, NavLink } from "react-router-dom";
 import container from "../containers/all.js";
 import _ from "lodash";
+import mapboxgl from 'mapbox-gl';
+import Mapbox from 'react-redux-mapbox-gl';
+//actions
+import searchHikes from "../actions/search_hikes.js";
+import searchRestaurants from "../actions/search_restaurants.js"
 
 class BrunchComponent extends React.Component {
   constructor(props) {
     super(props)
 
-
     this.handleYelpButton = this.handleYelpButton.bind(this)
-    this.getHike = this.getBrunch.bind(this)
+    this.getBrunch = this.getBrunch.bind(this)
+    this.getMap = this.getMap.bind(this)
+    this.mapStyle = this.mapStyle.bind(this)
+    this.mapOptions = this.mapOptions.bind(this)
+    this.putBrunchAreaOnTheMap = this.putBrunchAreaOnTheMap.bind(this)
   }
 
-  getBrunch(id) {
+  getBrunch() {
     // iterate through this.props.searchResult
-    console.log('brunches', this.props.reducer.foodSearchResults);
+    const id = this.props.match.params.id;
     const brunches = this.props.reducer.foodSearchResults;
     // https://lodash.com/docs/4.17.4#filter
     let theBrunch = _.filter(brunches, { 'id': id });
@@ -26,10 +34,101 @@ class BrunchComponent extends React.Component {
     console.log('go to yelp page');
   }
 
+  getMap(map) {
+    this.map = map;
+  }
+
+  mapStyle() {
+    return {
+      width: '98%',
+      height: '55%'
+    }
+  }
+
+  mapOptions(brunch) {
+    return {
+      style: 'mapbox://styles/mapbox/outdoors-v10',
+      center: brunch.coordinates,
+      zoom: 11
+    }
+  }
+
+  putBrunchAreaOnTheMap() {
+    this.map.on('load', function() {
+      console.log('put the hike on the map');
+
+      const hikes = this.props.reducer.searchResults;
+      const hikeFeatures = hikes.map(function(hike) {
+        const coordinates = [hike['lon'], hike['lat']];
+        const id = hike.unique_id;
+
+        return {
+          'type': 'Feature',
+          'properties': {
+            'id': id,
+            'type': 'hike',
+            'icon': 'triangle' // https://github.com/mapbox/mapbox-gl-styles
+          },
+          'geometry': {
+            'type': 'Point',
+            'coordinates': coordinates
+          }
+        }
+      });
+
+      const brunch = this.getBrunch();
+      const coordinates = brunch.coordinates;
+      const id = brunch['id'];
+      const brunchFeatures = [{
+        'type': 'Feature',
+        'properties': {
+          'id': id,
+          'type': 'brunch',
+          'icon': 'restaurant' // https://github.com/mapbox/mapbox-gl-styles
+        },
+        'geometry': {
+          'type': 'Point',
+          'coordinates': coordinates
+        }
+      }];
+
+      const features = hikeFeatures.concat(brunchFeatures);
+
+      this.map.addLayer({
+        "id": "places",
+        "type": "symbol",
+        "source": {
+            "type": "geojson",
+            "data": {
+              "type": "FeatureCollection",
+              "features": features
+            }
+        },
+        "layout": {
+          "icon-image": "{icon}-15",
+          "icon-allow-overlap": true,
+          "icon-size": 1
+        }
+      });
+
+      this.map.on('click', 'places', function (e) {
+        console.log('Clicked on a hike icon!');
+
+      }.bind(this));
+
+    }.bind(this));
+
+  }
+
+  componentDidMount() {
+    this.props.dispatch(searchHikes());
+    this.props.dispatch(searchRestaurants());
+
+    this.putBrunchAreaOnTheMap();
+  }
+
   render() {
-    console.log(this.props.match.params.id);
     const brunch = this.getBrunch(this.props.match.params.id);
-    console.log('brunch', brunch);
     return (
       <div className="brunch-card-container">
         <h1>Austin Adventures</h1>
@@ -38,6 +137,13 @@ class BrunchComponent extends React.Component {
         <p>Brunch: {brunch['brunch']}</p>
         <p>Review: {brunch['review']}</p>
         <button onClick={this.handleYelpButton} className="yelp-button" type="submit"><i className="fa fa-yelp" aria-hidden="true"></i></button>
+        <Mapbox
+          mapboxgl={mapboxgl}
+          accessToken="pk.eyJ1IjoiZGV2b25tb3VicnkiLCJhIjoiY2oyOXA1cGl4MDAwMjJ3b2djdjh4cmV2cyJ9.ZrmYtWukYTSnSRnDgUJlcQ"
+          style= {this.mapStyle()}
+          getMap={this.getMap}
+          options={this.mapOptions(brunch)} //mapbox://styles/mapbox/outdoors-v10
+        />
       </div>
     );
   }
