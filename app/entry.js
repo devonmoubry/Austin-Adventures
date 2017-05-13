34ed7c1e0b5ad6232e72ce16fcc142aff0857981
Devon Moubry <devon@moubry.com>
2017-05-13 17:40:11 -0500

    Remove photo requirements
    … for now

--- app/entry.js
+++ app/entry.js
@@ -1,9 +1,5 @@
 require('file-loader?emitFile=false!./index.html');
 require('./stylesheets/app.scss'); /* This loads the css and foces the sass to compile... */
-require('file-loader?emitFile=false!./images/averie-woodard-123973.jpg');
-require('file-loader?emitFile=false!./images/piyanut-suntaranil-223184.jpg');
-require('file-loader?emitFile=false!./images/aaron-burden-75599.jpg');
-require('file-loader?emitFile=false!./images/averie-woodard-123972.jpg');
 require('file-loader?emitFile=false!./images/favicon.ico');
 
 import app from './scripts/app.js';
