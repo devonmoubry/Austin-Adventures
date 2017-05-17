e92f49504039eb54695397b244341fcedc117f0d
Devon Moubry <devon@moubry.com>
2017-05-16 22:19:58 -0500

    Get foursquare data to render in brunch list

--- app/scripts/components/food_search_result.js
+++ app/scripts/components/food_search_result.js
@@ -5,11 +5,14 @@ import container from "../containers/all.js";
 //actions
 import favRestaurant from "../actions/fav_restaurant.js"
 
+import getFoursquareBrunchDetails from "../actions/get_foursquare_brunch_details.js"
+
 class FoodSearchResult extends React.Component {
   constructor(props) {
     super(props)
 
     this.handleFavRestaurant = this.handleFavRestaurant.bind(this)
+    this.state = {}
   }
 
   handleFavRestaurant(event) {
@@ -20,15 +23,30 @@ class FoodSearchResult extends React.Component {
     this.props.dispatch(favRestaurant(id, name, usertoken));
   }
 
+  componentDidMount() {
+    this.props.dispatch(getFoursquareBrunchDetails(this.props.restaurant.foursquare_id, function(data) {
+      this.setState({foursquareDetails: data.response.venue})
+    }.bind(this)));
+  }
+
   render() {
 
+    let brunchfoursquareDetails = <p>Loading details ...</p>;
+    if(this.state.foursquareDetails != undefined) {
+      const currency = "no price";
+      if (this.state.foursquareDetails.attributes.groups != undefined && this.state.foursquareDetails.attributes.groups.length > 0) {
+        currency = this.state.foursquareDetails.attributes.groups[0].summary
+      }
+      brunchfoursquareDetails = <li className="search-result">
+              <p tabIndex="0">{this.props.restaurant.name}</p>
+              <p>{currency}</p>
+              <p>{this.props.restaurant.website}</p>
+              <button className="submit-input" onClick={this.handleFavRestaurant} type="submit" value="Favorite"><i className="fa fa-heart-o" aria-hidden="true"></i></button>
+            </li>
+    }
+
     return(
-      <li className="search-result">
-        <p tabIndex="0">{this.props.restaurant.name}</p>
-        <p>{this.props.restaurant.price}</p>
-        <p>{this.props.restaurant.webiste}</p>
-        <button className="submit-input" onClick={this.handleFavRestaurant} type="submit" value="Favorite"><i className="fa fa-heart-o" aria-hidden="true"></i></button>
-      </li>
+      <div>{brunchfoursquareDetails}</div>
     )
   }
 }
